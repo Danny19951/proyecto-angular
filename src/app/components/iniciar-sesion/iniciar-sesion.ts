1a7cfe95e7d873cb29641841c6c9e42ce88ef5b0
DESKTOP-KEHG5MG\danie <danieljlm19951@gmail.com>
2026-04-24 20:46:26 -0500

    Cambios creados para cambiar el boton superior y permitir cerrar sesion

--- src/app/components/iniciar-sesion/iniciar-sesion.ts
+++ src/app/components/iniciar-sesion/iniciar-sesion.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -17,6 +19,10 @@ export class IniciarSesionComponent {
   errorMsg = '';
 
   iniciarSesion() {
+
+    var elementobotones = document.getElementsByClassName("navbar-user desktop-menu");
+    if (elementobotones != null) { elementobotones[0].innerHTML = '<a routerLink="/iniciar-sesion">Iniciar Sesión</a>'; }
+
     const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
     const usuario = usuarios.find((u: any) =>
       u.username === this.loginData.username && u.password === this.loginData.password
@@ -29,4 +35,10 @@ export class IniciarSesionComponent {
       this.errorMsg = 'Usuario o contraseña incorrectos';
     }
   }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 }
+
